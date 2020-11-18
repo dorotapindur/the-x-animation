cb8e25a4022a843888cf23d2ec51d2ef3219637a
Dorota Pindur <dpindur@wp.pl>
2020-11-18 23:17:51 +0100

    feat: x - vertical rotation

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const button = document.querySelector('.button--js');
 const options = document.querySelector('.options--js');
+const card = document.querySelector('.card-container--js');
 
 button.addEventListener('click', () => {
     button.classList.toggle('opened');
     options.classList.toggle('options--open');
+})
+
+card.addEventListener('click', () => {
+    card.classList.toggle('vertical');
 })

a295a51b503d271a22d2da47a84671a9f1822a7e
Dorota Pindur <dpindur@wp.pl>
2020-11-19 18:46:28 +0100

    fix: new approach - separate class names

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 const button = document.querySelector('.button--js');
 const options = document.querySelector('.options--js');
-const card = document.querySelector('.card-container--js');
-const vertical = document.querySelector('.vertical--js');
+const optionsHorizontal = document.querySelector('.options__horizontal--js');
+const optionsVertical = document.querySelector('.options__vertical--js');
 const horizontal = document.querySelector('.horizontal--js');
+const vertical = document.querySelector('.vertical--js');
+
+//const card = document.querySelector('.card-container--js');
+
 
 button.addEventListener('click', () => {
     button.classList.toggle('opened');
     options.classList.toggle('options--open');
 })
+optionsHorizontal.addEventListener('click', () => {
+    horizontal.classList.toggle('rotate-x');
+})
+optionsVertical.addEventListener('click', () => {
+    vertical.classList.toggle('rotate-y');
+})
 
+/*
 horizontal.addEventListener('click', () => {
-    card.classList.toggle('transform');
     document.documentElement.style.setProperty('--front', 'rotateX(-180deg)');
     document.documentElement.style.setProperty('--back', 'rotateX(0deg)');
     document.documentElement.style.setProperty('--transform-front', 'rotateX(0deg)');
     document.documentElement.style.setProperty('--transform-back', 'rotateX(180deg)');
+    card.classList.toggle('transform');
 })
 
 vertical.addEventListener('click', () => {
-    card.classList.toggle('transform');
     document.documentElement.style.setProperty('--front', 'rotateY(0deg)');
     document.documentElement.style.setProperty('--back', 'rotateY(180deg)');
     document.documentElement.style.setProperty('--transform-front', 'rotateY(-180deg)');
     document.documentElement.style.setProperty('--transform-back', 'rotateY(0deg)');
+    card.classList.toggle('transform');
 })
+*/

9c92c09789b4276737fb03c02feb9c8f0750d818
Dorota Pindur <dpindur@wp.pl>
2020-11-24 12:24:56 +0100

    feat: snake and slide code-snippet complete

--- index.js
+++ index.js
@@ -42,17 +42,150 @@ slide.addEventListener('click', () => {
 
 snakeCodeButton.addEventListener('click', () => {
     codeSource = snake.innerHTML;
-    htmlCodeSnippet.innerHTML='';
-    htmlCodeSnippet.textContent = `HTML: ${codeSource}`;
+    htmlCodeSnippet.innerHTML = '';
+    htmlCodeSnippet.textContent = 
+`HTML:
 
+<div class="snake open-snake snake--js"> 
+    ${codeSource}
+</div>`;
+    scssCodeSnippet.innerHTML = '';
+    scssCodeSnippet.innerHTML = 
+`SCSS:
+
+.snake {
+    width: 200px;
+    height: 200px;
+    position: relative;
+    margin: 0 auto 50px;
+    display: block;
+    stroke: black;
+    stroke-width: 3.175;
+    background: none;
+    border: none;
+    cursor: pointer;
+    font-size: 14px;
+    transition: stroke .3s ease-in;
+    &__shape {
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+    &__path {
+        transition: stroke-dasharray 1s cubic-bezier(0.68, 0.01, 0.08, 0.97), stroke-dashoffset 1s cubic-bezier(0.68, 0.01, 0.08, 0.97);
+        &--one {
+            stroke-dasharray: 29 29;
+            stroke-dashoffset: 0;
+        }
+        &--two {
+            stroke-dasharray: 29 169;
+            stroke-dashoffset: -139.5;
+        }
+        &--three {
+            stroke-dasharray: 29 172;
+            stroke-dashoffset: -170.5;
+        }
+    }
+}
+
+.open-snake .snake__path--one {
+    stroke-dasharray: 0 29;
+    stroke-dashoffset: -15;
+}
+.open-snake .snake__path--two {
+    stroke-dasharray: 29 199;
+    stroke-dashoffset: 0;
+}
+.open-snake .snake__path--three {
+    stroke-dasharray: 29 200;
+    stroke-dashoffset: 0;
+}
+`;
+    jsCodeSnippet.innerHTML = '';
+    jsCodeSnippet.innerHTML = 
+`JS:
+
+const snake = document.querySelector('.snake--js');
+
+snake.addEventListener('click', () => {
+    snake.classList.toggle('open-snake');
+})`;
     codeContainer.classList.add('code-container--opened');
 })
+
 slideCodeButton.addEventListener('click', () => {
     codeSource = slide.innerHTML;
     htmlCodeSnippet.innerHTML='';
-    htmlCodeSnippet.textContent = `HTML: ${codeSource}`;
+    htmlCodeSnippet.textContent = 
+`HTML:
+
+<div class="slide slide--js">
+${codeSource}
+</div>`;
+    scssCodeSnippet.innerHTML = '';
+    scssCodeSnippet.innerHTML = 
+`SCSS:
+
+.slide {
+    width: 200px;
+    height: 200px;
+    position: relative;
+    margin: 0 auto 50px;
+    display: block;
+    stroke: black;
+    stroke-width: 3.175;
+    background: none;
+    border: none;
+    cursor: pointer;
+    font-size: 14px;
+    transition: stroke .3s ease-in;
+    &__shape {
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+    &__path {
+        transition: stroke-dasharray 600ms cubic-bezier(0.4, 0, 0.2, 1), stroke-dashoffset 600ms cubic-bezier(0.4, 0, 0.2, 1);
+        &--one {
+            stroke-dasharray: 0 29;
+            stroke-dashoffset: -15;
+        }
+        &--two {
+            stroke-dasharray: 29 105;
+            stroke-dashoffset: 0;
+        }
+        &--three {
+            stroke-dasharray: 29 105;
+            stroke-dashoffset: 0;
+        }
+    }
+}
+
+.closed .slide__path--one {
+        stroke-dasharray: 29 29;
+        stroke-dashoffset: 0;
+}
+.closed .slide__path--two {
+        stroke-dasharray: 43 105;
+        stroke-dashoffset: -72;
+}
+.closed .slide__path--three {
+        stroke-dasharray: 43 105;
+        stroke-dashoffset: -72;
+}
+`;
+    jsCodeSnippet.innerHTML = '';
+    jsCodeSnippet.innerHTML = 
+`JS:
+
+const slide = document.querySelector('.slide--js');
+
+slide.addEventListener('click', () => {
+    slide.classList.toggle('closed');
+})`;
     codeContainer.classList.add('code-container--opened');
 })
+
 angleCodeButton.addEventListener('click', () => {
     codeSource = angle.innerHTML;
     htmlCodeSnippet.innerHTML='';
---
+++ snake-snippet.js
@@ -0,0 +1,82 @@
+export function snakeSnippet() {
+
+snakeCodeButton.addEventListener('click', () => {
+    codeSource = snake.innerHTML;
+    htmlCodeSnippet.innerHTML = '';
+    htmlCodeSnippet.textContent = 
+`HTML:
+
+<div class="snake open-snake snake--js"> 
+    ${codeSource}
+</div>`;
+    scssCodeSnippet.innerHTML = '';
+    scssCodeSnippet.innerHTML = 
+`SCSS:
+
+:root {
+    --color: #292828;
+    --x: #7e9401;
+}
+.snake {
+    width: 200px;
+    height: 200px;
+    position: relative;
+    margin: 0 auto 50px;
+    display: block;
+    stroke: var(--color);
+    stroke-width: 3.175;
+    background: none;
+    border: none;
+    cursor: pointer;
+    font-size: 14px;
+    transition: stroke .3s ease-in;
+    &:hover {
+        stroke: var(--x);
+    }   
+    &__shape {
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+    &__path {
+        transition: stroke-dasharray 1s cubic-bezier(0.68, 0.01, 0.08, 0.97), stroke-dashoffset 1s cubic-bezier(0.68, 0.01, 0.08, 0.97);
+        &--one {
+            stroke-dasharray: 29 29;
+            stroke-dashoffset: 0;
+        }
+        &--two {
+            stroke-dasharray: 29 169;
+            stroke-dashoffset: -139.5;
+        }
+        &--three {
+            stroke-dasharray: 29 172;
+            stroke-dashoffset: -170.5;
+        }
+    }
+}
+
+.open-snake .snake__path--one {
+    stroke-dasharray: 0 29;
+    stroke-dashoffset: -15;
+}
+.open-snake .snake__path--two {
+    stroke-dasharray: 29 199;
+    stroke-dashoffset: 0;
+}
+.open-snake .snake__path--three {
+    stroke-dasharray: 29 200;
+    stroke-dashoffset: 0;
+}
+`;
+    jsCodeSnippet.innerHTML = '';
+    jsCodeSnippet.innerHTML = 
+`JS:
+
+const snake = document.querySelector('.snake--js');
+
+snake.addEventListener('click', () => {
+    snake.classList.toggle('open-snake');
+})`;
+    codeContainer.classList.add('code-container--opened');
+})
+}
